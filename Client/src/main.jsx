b8d34b5dd814b02fc57ac854ca1c7e3a2563fca0
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2022-12-14 17:34:29 +0200

    Initial create base componetns

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
-import App from './App';
 import './index.css';
+
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
