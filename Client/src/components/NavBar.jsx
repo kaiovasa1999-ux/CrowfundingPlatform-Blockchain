5c72eccc1bd47b755762b16c45d62205442b9554
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2022-12-16 16:08:40 +0200

    sideBar implementation

--- Client/src/components/NavBar.jsx
+++ Client/src/components/NavBar.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import { Link,useNavigate } from 'react-router-dom';
+import {logo,sun} from '../assets';
+import { navlinks } from '../constants';
 const NavBar = () => {
   return (
     <div>
